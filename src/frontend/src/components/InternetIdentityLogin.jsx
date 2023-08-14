f5f074ec2fd3868cef779b1cfb5d03f4f2ec494e
Matheun Struik <matheunstruik@outlook.com>
2023-08-14 20:20:44 +0200

    Added a logged in state to the InternetIdentityLogin

--- src/frontend/src/components/InternetIdentityLogin.jsx
+++ src/frontend/src/components/InternetIdentityLogin.jsx
@@ -3,6 +3,7 @@ import { backend } from "../../../declarations/backend";
 import { AuthClient } from "@dfinity/auth-client";
 
 import Button from '@mui/material/Button';
+import 'webcrypto-shim';
 
 export default function InternetIdentityLogin() {
   const [user, setUser] = React.useState('');
@@ -24,15 +25,17 @@ export default function InternetIdentityLogin() {
 
       // call the greet method
       const greeting = await backend.greet(authClient.getIdentity().getPrincipal().toText());
+      console.log(authClient.getIdentity());
       setUser(greeting);
     } catch (error) {
       console.error(error);
     }
   }
 
-  return (
-    <>
-      <Button color="inherit" onClick={handleLogin}>Login</Button>
-    </>
+  if (user) return (
+    <Button color="inherit">Logged in</Button>
+  )
+  else return (
+    <Button color="inherit" onClick={handleLogin}>Login</Button>
   )
 }
